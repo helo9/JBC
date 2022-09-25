d1dd230de7742d0253f022cc799eb4f5ac41587c
Jonathan Hahn <helo9@users.noreply.github.com>
2022-09-25 19:33:24 +0200

    Create FIFO event queue in main function

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,22 @@
 #endif
 #include <DebounceFilter.hpp>
 #include <SwitchingRegulator.hpp>
+#include <RingBuffer.hpp>
 #include <Timer.hpp>
+#include <Events.hpp>
 #include <stdio.h>
+#include <assert.h>
+
+constexpr size_t event_queue_size = 20;
+
+void on_timer_event();
 
 SwitchingRegulator regulator_heater1(82, 80);
 SwitchingRegulator regulator_heater2(80, 75);
 
-volatile bool timer_event = false;
+Timer timer(on_timer_event);
 
-void set_timer_event() {
-    timer_event = true;
-}
-
-void reset_timer_event() {
-    timer_event = false;
-}
-
-Timer timer(set_timer_event);
-
-unsigned long counter = 1000;
+RingBuffer<Event, event_queue_size> event_queue;
 
 void setup(void) {
 
@@ -34,18 +31,32 @@ void setup(void) {
 }
 
 void loop(void) {
+
+    while(event_queue.count_free() == event_queue_size);
     
-    if (timer_event) {
-        
-        reset_timer_event();
+    Event current_event;
+    if (!event_queue.get(&current_event)) {
+        assert(0);
+    }
 
-        const float temperature = board::get_temperature_celsius();
-        
-        const bool heater1_on = regulator_heater1.calc_new_command(temperature);
-        board::set_heater1(heater1_on);
+    switch (current_event.type) {
+        case (EventTypes::timer): 
+        {
+            const float temperature = board::get_temperature_celsius();
+            
+            const bool heater1_on = regulator_heater1.calc_new_command(temperature);
+            board::set_heater1(heater1_on);
 
-        const bool heater2_on = regulator_heater2.calc_new_command(temperature);
-        board::set_heater2(heater2_on);
+            const bool heater2_on = regulator_heater2.calc_new_command(temperature);
+            board::set_heater2(heater2_on);
+            
+            break;
+        }
+        case (EventTypes::button): 
+        {
+            board::print("Irgendwas mit Button :)\n");
+            break;
+        }
     }
 }
 
@@ -54,8 +65,18 @@ void board_evt_handler::on_systick() {
 }
 
 void board_evt_handler::on_button_event(board_evt_handler::Button btn, board_evt_handler::ButtonEvent evt) {
-    char msg[50] = {};
-    snprintf(msg, 50, "%d %d\n", 
-        static_cast<int>(btn), static_cast<int>(evt));
-    board::print(msg);
+    const uint8_t button_no = static_cast<uint8_t>(btn);
+    const uint8_t button_evt = static_cast<uint8_t>(evt);
+
+    event_queue.put(Event {
+        EventTypes::button,
+        button_no,
+        button_evt
+    });
+}
+
+void on_timer_event() {
+    event_queue.put(Event {
+        EventTypes::timer, 0, 0
+    });
 }
